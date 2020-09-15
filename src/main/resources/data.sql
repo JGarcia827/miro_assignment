dca6ed53c231d073c3531bce724d890e025014ab
Jose Garcia <juniorgarcia@live.nl>
2020-09-15 14:57:53 +0200

    Add demo/test data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+--Demo and test data
+INSERT INTO widget (id, x_coordinate, y_coordinate, z_index, width, height, last_modified) VALUES (1, 1000, 1000, 0, 100, 100, '2020-01-01 15:00:15');
+INSERT INTO widget (id, x_coordinate, y_coordinate, z_index, width, height, last_modified) VALUES (2, 1000, 1050, 1, 100, 100, '2020-01-02 15:00:15');
+INSERT INTO widget (id, x_coordinate, y_coordinate, z_index, width, height, last_modified) VALUES (3, 1050, 1050, 3, 100, 100, '2020-01-03 15:00:15');
+INSERT INTO widget (id, x_coordinate, y_coordinate, z_index, width, height, last_modified) VALUES (4, 200, 10, 4, 10, 10, '2020-01-04 15:00:15');
+INSERT INTO widget (id, x_coordinate, y_coordinate, z_index, width, height, last_modified) VALUES (5, 200, 10, 10, 10, 10, '2020-01-05 15:00:15');
+INSERT INTO widget (id, x_coordinate, y_coordinate, z_index, width, height, last_modified) VALUES (6, 200, 10, 11, 10, 10, '2020-01-06 15:00:15');
+INSERT INTO widget (id, x_coordinate, y_coordinate, z_index, width, height, last_modified) VALUES (7, 200, 10, 14, 10, 10, '2020-01-07 15:00:15');
+INSERT INTO widget (id, x_coordinate, y_coordinate, z_index, width, height, last_modified) VALUES (8, 200, 10, 16, 10, 10, '2020-01-08 15:00:15');
+INSERT INTO widget (id, x_coordinate, y_coordinate, z_index, width, height, last_modified) VALUES (9, 200, 10, 20, 10, 10, '2020-01-09 15:00:15');
+INSERT INTO widget (id, x_coordinate, y_coordinate, z_index, width, height, last_modified) VALUES (10, 200, 10, 21, 10, 10, '2020-01-10 15:00:15');
